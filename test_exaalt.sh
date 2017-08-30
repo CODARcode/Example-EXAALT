762f0b8984f6bd6937ab8936dc883ec9766580ae
tkurc <tkurc>
2017-08-30 15:58:11 -0400

    .

--- test_exaalt.sh
+++ test_exaalt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mpirun -np 8 /home/tkurc/codar/Example-Heat_Transfer/stage_write/stage_write  output.bp staged.bp FLEXPATH "" MPI "" &
-mpirun -np 8 ./pt_producer_global states_list.txt 128 128 output.bp FLEXPATH "" 
+mpirun -np 8 ./pt_producer_global states_list.txt 128 output.bp FLEXPATH "" 
