8913c955ed86a53704c9a0a91fc7c81dd12a02de
tkurc <tkurc>
2017-08-30 17:13:59 -0400

    Modified stage_write to output to a new file every 5K time steps.

--- local/test_exaalt.sh
+++ local/test_exaalt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mpirun -np 8 /home/tkurc/codar/Example-Heat_Transfer/stage_write/stage_write  output.bp staged.bp FLEXPATH "" MPI "" &
-mpirun -np 8 ./pt_producer_global states_local.txt 128 output.bp FLEXPATH "" 
+mpirun -np 8 ../stage_write/stage_write output.bp staged.bp FLEXPATH "" MPI "" &
+mpirun -np 8 ../pt_producer_global states_local.txt 1024 output.bp FLEXPATH "" 
